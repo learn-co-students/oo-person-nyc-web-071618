a204b78c1074c66f24d9a2f853918cef23e7a1fa
Ena Anicama <ena.v825@gmail.com>
2018-07-18 18:06:18 -0400

    WIP-2 - oo-person-07-18-18-ea

--- lib/person.rb
+++ lib/person.rb
@@ -4,12 +4,13 @@ require 'pry'
 class Person
   attr_reader :name
   # attr_writer
-  attr_accessor :happiness, :bank_account
+  attr_accessor :happiness, :bank_account, :hygiene
 
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
+    @hygiene = 8
   end
 
   def happiness=(index)
@@ -21,5 +22,44 @@ class Person
       @happiness = index
     end
   end
+
+  def hygiene=(index)
+    if index > 10
+      @hygiene = 10
+    elsif index < 0
+      @hygiene = 0
+    else
+      @hygiene = index
+    end
+  end
+
+  def happy?
+    if @happiness > 7
+      true
+    else
+      false
+    end
+  end
+
+  def clean?
+    if @hygiene > 7
+      true
+    else
+      false
+    end
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    @hygiene += 4
+    # hygiene=(@hygiene)
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+
 
 end
