2eddfbc0e13f64f9c21dd6ab11254b79c1f60141
cmonkey03 <russellpbrown@gmail.com>
2018-07-18 17:19:53 -0400

    Happiness index added

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,25 @@
 # your code goes here
+
+require 'pry'
 class Person
   attr_reader :name
-  attr_writer :bank_account
-  attr_accessor :happiness_index
+  # attr_writer
+  attr_accessor :happiness, :bank_account
 
   def initialize(name)
     @name = name
-    @bank_account
-  end
-
-  def bank_account
     @bank_account = 25
+    @happiness = 8
   end
 
-  def happiness
-    @happiness_index = 8
-    
+  def happiness=(index)
+    if index > 10
+      @happiness = 10
+    elsif index < 0
+      @happiness = 0
+    else
+      @happiness = index
+    end
   end
+
 end
