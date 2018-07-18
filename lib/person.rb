84a7a6afc5215591d6c0010f7b77f948cd76e7b8
Your Name <you@example.com>
2018-07-18 18:07:45 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,9 @@ require 'pry'
 
 class Person
 
-  attr_accessor :bank_account, :happiness, :hygiene
+  attr_accessor :bank_account, :happiness, :hygiene, :take_bath
   attr_reader :name
+  attr_writer
 
   def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
     @name = name
@@ -33,7 +34,57 @@ class Person
     end
   end
 
+
+  def happy?
+    if @happiness > 7
+      true
+    else
+      false
+  end
+ end
+ def clean?
+   if @hygiene > 7
+     true
+   else
+     false
+   end
+ end
+
+ def get_paid(salary)
+   @bank_account += salary
+   "all about the benjamins"
+ end
+
+ def take_bath
+  self.hygiene += 4
+   "♪ Rub-a-dub just relaxing in the tub ♫"
+ end
+
+  def work_out
+    self.happiness += 2
+   self.hygiene -= 3
+   "♪ another one bites the dust ♫"
+ end
+ def call_friend(friend)
+   self.happiness +=3
+   friend.happiness += 3
+   "Hi #{friend.name}! It's #{self.name}. How are you?"
+   #
+ end
+
+ def start_conversation(name, topic)
+   if topic == "politics"
+     name.happiness -=2
+     self.happiness -= 2
+     "blah blah partisan blah lobbyist"
+   elsif topic == "weather"
+     name.happiness += 1
+     self.happiness += 1
+     "blah blah sun blah rain"
+   else
+     "blah blah blah blah blah"
+   end
+ end
 end
 
-bob = Person.new("Bob")
-# binding.pry
+### end of the comment ##
